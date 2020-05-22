b43c6ec423da0debe0dba29bf16c9071afafc4e8
Andrejs Agejevs <andrejs.agejevs@ideaportriga.lv>
2020-05-22 15:00:45 +0300

    Recoil

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react'
+import * as React from 'react'
+import { RecoilRoot } from 'recoil'
+import { stateA, stateB } from '../recoil/atoms'
+import Counter from './Counter'
+import Summa from './Summa'
 import '../styles/app.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const App = () => (
+  <RecoilRoot>
     <div className="app">
-      <button onClick={() => setCount(count + 1)}>Counter: {count}</button>
+      <Counter stateAtom={stateA} />
+      <Counter stateAtom={stateB} />
+      <Summa />
     </div>
-  )
-}
+  </RecoilRoot>
+)
 
 export default App
